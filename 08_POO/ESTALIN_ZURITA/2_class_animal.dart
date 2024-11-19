34747e288e24c1527eb9f10c1f2a1e52a1af2ec2
estalinzurita <estalinzurita@gmail.com>
2024-11-18 20:36:41 -0500

    add: comentarios

---
+++ 08_POO/ESTALIN_ZURITA/2_class_animal.dart
@@ -0,0 +1,24 @@
+void main () {
+
+final perro = Animal (
+
+  nombre: "Pancho",
+  edad: 4,
+  
+);
+
+final gato = Animal (
+
+  nombre: "Paco"
+  edad: 3,
+)
+}
+
+/// Clase Animal
+
+
+class Animal { 
+
+
+
+}
